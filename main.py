4eb6b313390b8e5e93e3a935328c669d81a17a56
Satheesh_A <satheeshkriya5@gmail.com>
2026-09-25 09:43:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Master Entrypoint for Polyphonic Music Melody Generation Based on Inverse Reinforcement Learning (ST-214)
+Executes the unified 12-step end-to-end workflow on the MAESTRO MIDI dataset.
+"""
+
+from polyphonic_irl_complete import run_pipeline, main
+
+if __name__ == "__main__":
+    main()
